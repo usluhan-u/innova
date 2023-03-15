aace625f75359a52916721f3ccd3164757f29134
Usluhan Y. Uslu <81639028+in-vp@users.noreply.github.com>
2023-03-15 15:35:49 +0300

    Url-rollback

--- next.config.js
+++ next.config.js
@@ -8,7 +8,6 @@ module.exports = {
   images: {
     domains: [
       'localhost',
-      'https://p01--innova-panel--wqf7yvvlslgm.code.run/',
       // Your domain(s) here
     ],
     deviceSizes: sizes,
