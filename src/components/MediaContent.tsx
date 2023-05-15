cd81e3dcb8fed6c63cc1963c1caa8ef7c51d1722
mumincelal <mumincelalpinar@gmail.com>
2023-05-15 18:18:56 +0300

    fix: solve media content

--- src/components/MediaContent.tsx
+++ src/components/MediaContent.tsx
@@ -4,6 +4,7 @@ import { FiArrowRight } from 'react-icons/fi';
 import { MediaContentType } from '../blocks';
 import { RichText } from './RichText';
 import { TextIconCallToAction } from './TextIconCallToAction';
+import { Template } from './Template';
 
 export interface MediaContentProps extends MediaContentType {}
 
@@ -11,63 +12,70 @@ export const MediaContent = ({
   content,
   contentPosition,
   image,
-  callToAction
+  callToAction,
+  backgroundColor,
+  width
 }: MediaContentProps) => (
-  <Grid
-    templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}
-    w="full"
-    gap={8}
-  >
-    {contentPosition === 'left' && (
-      <>
-        <GridItem>
-          <Image
-            objectFit="cover"
-            src={image.url}
-            alt={image.alt}
-            boxSize="full"
-            borderRadius="lg"
-          />
-        </GridItem>
-        <GridItem>
-          <Flex flexDir="column" justify="space-between">
-            <RichText content={content} />
-            {callToAction && Object.keys(callToAction).length > 0 && (
-              <TextIconCallToAction
-                {...callToAction}
-                color="text.blue"
-                icon={FiArrowRight}
-              />
-            )}
-          </Flex>
-        </GridItem>
-      </>
-    )}
+  <Template backgroundColor={backgroundColor} width={width}>
+    <Grid
+      templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}
+      alignItems={{ base: 'normal', md: 'center' }}
+      w="full"
+      gap={8}
+    >
+      {contentPosition === 'right' && (
+        <>
+          <GridItem>
+            <Image
+              objectFit="cover"
+              src={image.url}
+              alt={image.alt}
+              w="full"
+              h={{ base: 'full', md: 'md' }}
+              borderRadius="lg"
+            />
+          </GridItem>
+          <GridItem>
+            <Flex flexDir="column" justify="space-between">
+              <RichText content={content} />
+              {callToAction && Object.keys(callToAction).length > 0 && (
+                <TextIconCallToAction
+                  {...callToAction}
+                  color="text.blue"
+                  icon={FiArrowRight}
+                />
+              )}
+            </Flex>
+          </GridItem>
+        </>
+      )}
 
-    {contentPosition === 'right' && (
-      <>
-        <GridItem>
-          <Flex flexDir="column" justify="space-between" boxSize="full">
-            <RichText content={content} />
-            {callToAction && Object.keys(callToAction).length > 0 && (
-              <TextIconCallToAction
-                {...callToAction}
-                color="text.blue"
-                icon={FiArrowRight}
-              />
-            )}
-          </Flex>
-        </GridItem>
-        <GridItem>
-          <Image
-            objectFit="cover"
-            src={image.url}
-            alt={image.alt}
-            borderRadius="lg"
-            boxSize="full"
-          />
-        </GridItem>
-      </>
-    )}
-  </Grid>
+      {contentPosition === 'left' && (
+        <>
+          <GridItem>
+            <Flex flexDir="column" justify="space-between" boxSize="full">
+              <RichText content={content} />
+              {callToAction && Object.keys(callToAction).length > 0 && (
+                <TextIconCallToAction
+                  {...callToAction}
+                  color="text.blue"
+                  icon={FiArrowRight}
+                />
+              )}
+            </Flex>
+          </GridItem>
+          <GridItem>
+            <Image
+              objectFit="cover"
+              src={image.url}
+              alt={image.alt}
+              borderRadius="lg"
+              w="full"
+              h={{ base: 'full', md: 'md' }}
+            />
+          </GridItem>
+        </>
+      )}
+    </Grid>
+  </Template>
 );
